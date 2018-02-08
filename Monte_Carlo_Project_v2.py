62456884e7f955a7588e035d265e547ad0f62f94
Spasimir Nonev <snonev@uni-sofia.bg>
2018-02-08 19:08:25 +0200

    Add files via upload

--- Monte_Carlo_Project_v2.py
+++ Monte_Carlo_Project_v2.py
@@ -1,8 +1,14 @@
+
+# coding: utf-8
+
+# In[3]:
+
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import random
 
+sample_size = 10
 grid_size = 20
 subgrid_size = int(grid_size/4)
 
@@ -15,8 +21,28 @@ def split(arr, size):
     arrs.append(arr)
     return arrs
 
-class world:
+
+class robot:
     
+    def __init__(self):
+        self.sample = []
+        self.weights = []
+        
+        
+    
+    def sense(self, coords):
+        
+        
+        
+    def resample(self):
+        new = np.random.choice(self.sample, 5, p=[0.5, 0.1, 0.1, 0.3])
+        self.sample = new
+        return new
+        
+    def policy(self):
+    
+
+class world:
     
     def __init__(self, size, region_size):
         self.x=[]
@@ -34,8 +60,10 @@ class world:
         self.robots=["robot()","robot()","robot()"]
         for k in range(len(self.robots)):
             self.robot_locations.append([np.random.randint(0,grid_size),np.random.randint(0,grid_size)])
+            
     def get_grid_color(self):
         return self.grid_colors
+    
     def get_coordinates(self):
         return [self.x, self.y]
 
@@ -62,4 +90,36 @@ plt.plot(grid_coords[0],grid_coords[1], 'sy', markersize=20, mfc='none')
 for i in range(grid_size):
     for j in range(grid_size):
         plt.plot(i, j, 'sy', markersize=20, mfc=('none' if coords[i][j] == "B" else '#d3d3d3'))
+        
+plt.axis('off')        
 plt.show()
+
+
+# In[16]:
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+get_ipython().magic('matplotlib')
+
+N = 15
+# make an empty data set
+data = np.ones((N, N)) * np.nan
+# fill in some fake data
+for j in range(3)[::-1]:
+    data[N//2 - j : N//2 + j +1, N//2 - j : N//2 + j +1] = j
+# make a figure + axes
+fig, ax = plt.subplots(1, 1, tight_layout=True)
+# make color map
+my_cmap = matplotlib.colors.ListedColormap(['r', 'g', 'b'])
+# set the 'bad' values (nan) to be white and transparent
+my_cmap.set_bad(color='w', alpha=0)
+# draw the grid
+for x in range(N + 1):
+    ax.axhline(x, lw=2, color='k', zorder=5)
+    ax.axvline(x, lw=2, color='k', zorder=5)
+# draw the boxes
+ax.imshow(data, interpolation='none', cmap=my_cmap, extent=[0, N, 0, N], zorder=0)
+# turn off the axis labels
+ax.axis('off')
+
